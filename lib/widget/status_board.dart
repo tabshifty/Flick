19df3cf5372a64cdf59a40e409230add7371842e
LevelOnePlayer <57022592+tabshifty@users.noreply.github.com>
2025-01-16 01:31:43 +0000

    init statusboard

---
+++ lib/widget/status_board.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StatusBoard extends StatefulWidget {
+  final double value
+  const StatusBoard({ super.key, required this.value })
+  @override
+  State<StatusBoard> createState() => _StatusBoardState();
+}
+
+class _StatusBoardState extends State<StatusBoard> {
+ 
+}
